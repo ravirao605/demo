e167e3ca520c97fd16b7313d717b91e90e83e88c
ravir <ravirao605@gmail.com>
2022-08-21 18:35:02 -0700

    Added feature 4

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -16,9 +16,12 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	public static void feature4() {
+		System.out.println("Did something!");
+	}
 
 	public static void doSomething() {
-		System.out.println("Did something!");
+		System.out.println("Did something here!");
 	}
 	@Bean
 	CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
